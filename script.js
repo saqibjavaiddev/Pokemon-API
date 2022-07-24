eb2f274cdf99ac7c405b7405e37f2f0c30309ab3
msaqibjavaid <duckyprogrammer@gmail.com>
2022-07-24 23:44:12 +0500

    fixed JS linters

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
-const poke_container = document.querySelector('#poke_container');
-const pokemons_number = 150;
+const Pokecontainer = document.querySelector('#poke_container');
+const Pokemonsnumber = 150;
+
+const createpokemoncard = (Pokemon) => {
+  const PokemonEl = document.createElement('div');
+  PokemonEl.classList.add('pokemon');
+  const name = Pokemon.name[0].toUpperCase() + Pokemon.name.slice(1);
+  const poketype = Pokemon.types.map((type) => type.type.name);
+
+  const pokeinnerHTML = `
+      <div class="img-container">
+      <img src="https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${Pokemon.id}.svg">
+      </div>
+      <div class="info">
+          <span class="number">${Pokemon.id}</span>
+          <h3 class="name">${name}</h3>
+          <small class="type">Type: <span>${poketype[0]}</span></small>
+      </div>
+      `;
+
+  PokemonEl.innerHTML = pokeinnerHTML;
+  Pokecontainer.appendChild(PokemonEl);
+};
 
 const getpokemon = async (id) => {
-    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-    const res = await fetch(url);
-    const pokemon = await res.json();
-    createpokemoncard(pokemon);
-}
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const res = await fetch(url);
+  const Pokemon = await res.json();
+  createpokemoncard(Pokemon);
+};
 
-const fetchpokemons = async() => {
-    for (let i=1 ; i <= pokemons_number ; i++) {
-        await getpokemon(i);
-    }
-}
+const fetchpokemons = async () => {
+  for (let i = 1; i <= Pokemonsnumber; i += 1) {
+    getpokemon(i);
+  }
+};
 
 fetchpokemons();
-
-const createpokemoncard = (pokemon) => {
-    const pokemonEl = document.createElement('div');
-    pokemonEl.classList.add('pokemon');
-    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
-    const poke_type = pokemon.types.map(type => type.type.name);
-
-    const pokeinnerHTML = `
-    <div class="img-container">
-    <img src="https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${pokemon.id}.svg">
-    </div>
-    <div class="info">
-        <span class="number">${pokemon.id}</span>
-        <h3 class="name">${name}</h3>
-        <small class="type">Type: <span>${poke_type[0]}</span></small>
-    </div>
-    `;
-
-    pokemonEl.innerHTML = pokeinnerHTML;
-    poke_container.appendChild(pokemonEl);
-}
